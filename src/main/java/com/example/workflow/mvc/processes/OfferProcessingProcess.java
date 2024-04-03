a3f19c9939928b79414c80aca3e73fabbf85327f
unknown <tomasz.feliks.mlynarczyk@gmail.com>
2024-04-03 12:17:00 +0200

    UtworzenieDokumentuDelegate

--- src/main/java/com/example/workflow/mvc/processes/OfferProcessingProcess.java
+++ src/main/java/com/example/workflow/mvc/processes/OfferProcessingProcess.java
@@ -1,6 +1,7 @@
 package com.example.workflow.mvc.processes;
 
 public  class OfferProcessingProcess {
+    public static String L_ZAKS_DOSTAW="liczbaZaksiegowanychDostaw";
 
     public static String IS_OFFER_APPLICABLE="isOfferApplicable";
 }
