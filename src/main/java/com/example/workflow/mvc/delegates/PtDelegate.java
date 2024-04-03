18f69f37a77eeb684e08533f664e5ae1cdbbe9da
unknown <tomasz.feliks.mlynarczyk@gmail.com>
2024-04-03 12:06:41 +0200

    pt1

--- src/main/java/com/example/workflow/mvc/delegates/PtDelegate.java
+++ src/main/java/com/example/workflow/mvc/delegates/PtDelegate.java
@@ -1,15 +1,21 @@
 package com.example.workflow.mvc.delegates;
 
+import org.camunda.bpm.engine.HistoryService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PtDelegate implements JavaDelegate {
 
+    @Autowired HistoryService myHistoryService;
+
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception{
         var s = delegateExecution.getVariable("Input_pt1");
         delegateExecution.setVariableLocal("zmienna_pt1", "OUT:" + s);
+
+       // myHistoryService.createHistoricJobLogQuery().
     }
 }
