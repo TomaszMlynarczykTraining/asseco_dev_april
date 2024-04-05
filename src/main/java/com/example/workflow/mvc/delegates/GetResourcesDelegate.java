cd472cda954890db002f5dd5f4f6391e4c701cb3
unknown <tomasz.feliks.mlynarczyk@gmail.com>
2024-04-05 12:06:38 +0200

    messages

--- src/main/java/com/example/workflow/mvc/delegates/GetResourcesDelegate.java
+++ src/main/java/com/example/workflow/mvc/delegates/GetResourcesDelegate.java
@@ -2,10 +2,12 @@ package com.example.workflow.mvc.delegates;
 
 import com.example.workflow.mvc.processes.OfferProcessingProcess;
 import org.camunda.bpm.engine.HistoryService;
+import org.camunda.bpm.engine.ManagementService;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.rest.JobRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -22,6 +24,9 @@ public class GetResourcesDelegate implements JavaDelegate {
     @Autowired
     public HistoryService historyService;
 
+    @Autowired
+    public ManagementService managementService;
+
     private static String IS_OFFER_APPLICABLE="isOfferApplicable";
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
@@ -32,6 +37,8 @@ public class GetResourcesDelegate implements JavaDelegate {
         System.out.println(processDefinitionId);
         Boolean variable = (boolean)delegateExecution.getVariable(IS_OFFER_APPLICABLE);
 
+
+         //managementService.createJobQuery()..activityId("").list();
         //Wykonanie logiki biznesowej
 
         //Ustawienie potrzebnych zmiennych procesowych
