e2fec85aa97d9e6e76d60e7c2e1ee3d4a9342734
jaroslaw <jaroslaw.lewandowski@asseco.pl>
2024-04-05 15:18:51 +0200

    JL. delegate

--- src/main/java/com/example/workflow/mvc/delegates/grupa4/LoanDelegate.java
+++ src/main/java/com/example/workflow/mvc/delegates/grupa4/LoanDelegate.java
@@ -1,2 +1,26 @@
-package com.example.workflow.mvc.delegates.grupa4;public class LoanDelegate {
+package com.example.workflow.mvc.delegates.grupa4;
+
+import com.example.workflow.mvc.entity.Loan;
+import com.example.workflow.mvc.service.LoanService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LoanDelegate implements JavaDelegate {
+
+    @Autowired
+    private LoanService loanService;
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        List<Long> loanIdList = loanService.getAllLoans()
+                .stream()
+                .map( s -> s.getId())
+                .collect(Collectors.toList());
+        delegateExecution.setVariable("idList", loanIdList);
+
+    }
 }
