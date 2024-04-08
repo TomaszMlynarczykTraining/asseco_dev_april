a05d267c7068a75ed09fc299ca568d2d42e44dce
jaroslaw <jaroslaw.lewandowski@asseco.pl>
2024-04-08 16:03:55 +0200

    async delegates

--- src/main/java/com/example/workflow/mvc/delegates/grupa4/Service1Delegate.java
+++ src/main/java/com/example/workflow/mvc/delegates/grupa4/Service1Delegate.java
@@ -2,10 +2,12 @@ package com.example.workflow.mvc.delegates.grupa4;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Service1Delegate implements JavaDelegate {
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
-        throw new RuntimeException("Taki b")
+        System.out.println("Bylem w service1");
     }
 }
--- src/main/java/com/example/workflow/mvc/delegates/grupa4/Service2Delegate.java
+++ src/main/java/com/example/workflow/mvc/delegates/grupa4/Service2Delegate.java
@@ -1,2 +1,14 @@
-package com.example.workflow.mvc.delegates.grupa4;public class Service2Delegate {
+package com.example.workflow.mvc.delegates.grupa4;
+
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Service2Delegate  implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        throw new RuntimeException("Drugi blad");
+    }
 }
